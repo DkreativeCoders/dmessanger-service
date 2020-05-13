c103cbb20c75d2f7ae056b56b0bdd121ba88e2ff
Daniel Dada <ddada@teamapt.com>
2020-05-13 16:45:04 +0100

    Added courier model still going to add other properties

--- newstructure/domain/courier.go
+++ newstructure/domain/courier.go
@@ -2,8 +2,11 @@ package domain
 
 import "github.com/jinzhu/gorm"
 
-type Customer struct {
+type Courier struct {
 	gorm.Model
 	UserID uint
 	User User  `gorm:"foreignkey:UserID"`
+	Rating int
+	NumberOfRides int
+	NumberOfCompletedRides int
 }
