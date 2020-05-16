b2bc1d7e17574adb089de4f88f7eabae62ff1abd
Mayowa Aj <sagaraaj69@gmail.com>
2020-05-16 17:43:43 +0100

    basic swagger setup for api documentation

--- pkg/server/main.go
+++ pkg/server/main.go
@@ -1,3 +1,19 @@
+// De-messenger
+//
+// De-messenger
+//
+//     Schemes: http
+//     Host: localhost:8900
+//     Version: 0.0.1
+//	   BasePath: /api
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+// swagger:meta
 package main
 
 import (
