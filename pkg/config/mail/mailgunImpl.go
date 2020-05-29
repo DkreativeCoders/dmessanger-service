7f821f47c451d49fa1fc5cedeb3e6597c95a1e43
Daniel Dada <32602984+danieloluwadare@users.noreply.github.com>
2020-05-29 21:07:39 +0100

    removed unnecessary comments -2

--- pkg/config/mail/mailgunImpl.go
+++ pkg/config/mail/mailgunImpl.go
@@ -9,23 +9,6 @@ import (
 	"time"
 )
 
-func SendSimpleMessage(domain, apiKey string) (string, error) {
-	mg := mailgun.NewMailgun(domain, apiKey)
-	m := mg.NewMessage(
-		"Excited User <mailgun@YOUR_DOMAIN_NAME>",
-		"Hello",
-		"Testing some Mailgun awesomeness!",
-		"YOU@YOUR_DOMAIN_NAME",
-	)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
-	defer cancel()
-
-	_, id, err := mg.Send(ctx, m)
-	return id, err
-}
-
-
 type mailGunImplementation struct {
 	domain string
 	apiKey string
